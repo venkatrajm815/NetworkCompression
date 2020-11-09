6006030a1cf961fe93483213d543f813249ebb06
btshensfc <47489688+btshensfc@users.noreply.github.com>
2020-11-09 12:35:47 -0800

    Update standalone.c

--- standalone.c
+++ standalone.c
@@ -20,6 +20,237 @@
 #include "util.h"
 #define BUFFER_SIZE 2000
 
+//Creating number for IP header
+uint16_t checksum(uint16_t *addr, int len) {
+    int count = len;
+    register uint32_t sum = 0;
+    uint16_t answer = 0;
+
+    while(count > 1){
+        sum += *(addr++); 
+        count -= 2;
+    }
+
+    if(count > 0) {
+        sum += *(uint8_t *)addr;
+    }
+
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+    answer = ~sum;
+    return (answer);
+}
+
+//this function creates unique checksum for the UDP headers
+uint16_t udp4_checksum(struct ip iphdr, struct udphdr udphdr, uint8_t *payload, int payloadlen) {
+    char buf[IP_MAXPACKET];
+    char *ptr;
+    int chksumlen = 0;
+    int i;
+
+    ptr = &buf[0];  // ptr points to beginning of buffer buf
+
+    // Copy source IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
+    ptr += sizeof (iphdr.ip_src.s_addr);
+    chksumlen += sizeof (iphdr.ip_src.s_addr);
+
+    // Copy destination IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
+    ptr += sizeof (iphdr.ip_dst.s_addr);
+    chksumlen += sizeof (iphdr.ip_dst.s_addr);
+
+    // Copy zero field to buf (8 bits)
+    *ptr = 0; ptr++;
+    chksumlen += 1;
+
+    // Copy transport layer protocol to buf (8 bits)
+    memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
+    ptr += sizeof (iphdr.ip_p);
+    chksumlen += sizeof (iphdr.ip_p);
+
+    // Copy UDP length to buf (16 bits)
+    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+    ptr += sizeof (udphdr.len);
+    chksumlen += sizeof (udphdr.len);
+
+    // Copy UDP source port to buf (16 bits)
+    memcpy (ptr, &udphdr.source, sizeof (udphdr.source));
+    ptr += sizeof (udphdr.source);
+    chksumlen += sizeof (udphdr.source);
+
+    // Copy UDP destination port to buf (16 bits)
+    memcpy (ptr, &udphdr.dest, sizeof (udphdr.dest));
+    ptr += sizeof (udphdr.dest);
+    chksumlen += sizeof (udphdr.dest);
+
+    // Copy UDP length again to buf (16 bits)
+    memcpy (ptr, &udphdr.len, sizeof (udphdr.len));
+    ptr += sizeof (udphdr.len);
+    chksumlen += sizeof (udphdr.len);
+
+    // Copy UDP checksum to buf (16 bits)
+    // Zero, since we don't know it yet
+    *ptr = 0; ptr++;
+    *ptr = 0; ptr++;
+    chksumlen += 2;
+
+    // Copy payload to buf
+    memcpy (ptr, payload, payloadlen);
+    ptr += payloadlen;
+    chksumlen += payloadlen;
+
+    return checksum ((uint16_t *) buf, chksumlen);
+}
+
+//this function creates unique checksum for the TCP headers
+uint16_t tcp4_checksum (struct ip iphdr, struct tcphdr tcphdr) {
+    uint16_t svalue;
+    char buf[IP_MAXPACKET], cvalue;
+    char *ptr;
+    int chksumlen = 0;
+
+    // ptr points to beginning of buffer buf
+    ptr = &buf[0];
+
+    // Copy source IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_src.s_addr, sizeof (iphdr.ip_src.s_addr));
+    ptr += sizeof (iphdr.ip_src.s_addr);
+    chksumlen += sizeof (iphdr.ip_src.s_addr);
+
+    // Copy destination IP address into buf (32 bits)
+    memcpy (ptr, &iphdr.ip_dst.s_addr, sizeof (iphdr.ip_dst.s_addr));
+    ptr += sizeof (iphdr.ip_dst.s_addr);
+    chksumlen += sizeof (iphdr.ip_dst.s_addr);
+
+    // Copy zero field to buf (8 bits)
+    *ptr = 0; ptr++;
+    chksumlen += 1;
+
+    // Copy transport layer protocol to buf (8 bits)
+    memcpy (ptr, &iphdr.ip_p, sizeof (iphdr.ip_p));
+    ptr += sizeof (iphdr.ip_p);
+    chksumlen += sizeof (iphdr.ip_p);
+
+    // Copy TCP length to buf (16 bits)
+    svalue = htons (sizeof (tcphdr));
+    memcpy (ptr, &svalue, sizeof (svalue));
+    ptr += sizeof (svalue);
+    chksumlen += sizeof (svalue);
+
+    // Copy TCP source port to buf (16 bits)
+    memcpy (ptr, &tcphdr.th_sport, sizeof (tcphdr.th_sport));
+    ptr += sizeof (tcphdr.th_sport);
+    chksumlen += sizeof (tcphdr.th_sport);
+
+    // Copy TCP destination port to buf (16 bits)
+    memcpy (ptr, &tcphdr.th_dport, sizeof (tcphdr.th_dport));
+    ptr += sizeof (tcphdr.th_dport);
+    chksumlen += sizeof (tcphdr.th_dport);
+
+    // Copy sequence number to buf (32 bits)
+    memcpy (ptr, &tcphdr.th_seq, sizeof (tcphdr.th_seq));
+    ptr += sizeof (tcphdr.th_seq);
+    chksumlen += sizeof (tcphdr.th_seq);
+
+    // Copy acknowledgement number to buf (32 bits)
+    memcpy (ptr, &tcphdr.th_ack, sizeof (tcphdr.th_ack));
+    ptr += sizeof (tcphdr.th_ack);
+    chksumlen += sizeof (tcphdr.th_ack);
+
+    // Copy data offset to buf (4 bits) and
+    // copy reserved bits to buf (4 bits)
+    cvalue = (tcphdr.th_off << 4) + tcphdr.th_x2;
+    memcpy (ptr, &cvalue, sizeof (cvalue));
+    ptr += sizeof (cvalue);
+    chksumlen += sizeof (cvalue);
+
+    // Copy TCP flags to buf (8 bits)
+    memcpy (ptr, &tcphdr.th_flags, sizeof (tcphdr.th_flags));
+    ptr += sizeof (tcphdr.th_flags);
+    chksumlen += sizeof (tcphdr.th_flags);
+
+    // Copy TCP window size to buf (16 bits)
+    memcpy (ptr, &tcphdr.th_win, sizeof (tcphdr.th_win));
+    ptr += sizeof (tcphdr.th_win);
+    chksumlen += sizeof (tcphdr.th_win);
+    return checksum ((uint16_t *) buf, chksumlen);
+}
+
+// Allocate memory for an array of chars
+char * allocate_strmem(int len) {
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf(stderr, "ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp = (char *)malloc(len * sizeof (char));
+    if (tmp != NULL){
+        memset(tmp, 0, len * sizeof (char));
+        return(tmp);
+    } else {
+        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_strmem().\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Allocate memory for an array of unsigned chars.
+uint8_t * allocate_ustrmem(int len) {
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_ustrmem().\n", len);
+        exit (EXIT_FAILURE);
+    }
+
+    tmp = (uint8_t *) malloc (len * sizeof (uint8_t));
+    if (tmp != NULL) {
+        memset (tmp, 0, len * sizeof (uint8_t));
+        return (tmp);
+    } else {
+        fprintf (stderr, "ERROR: Cannot allocate memory for array allocate_ustrmem().\n");
+        exit (EXIT_FAILURE);
+    }
+}
+
+// Allocate memory for an array of ints.
+int * allocate_intmem(int len) {
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf (stderr, "ERROR: Cannot allocate memory because len = %i in allocate_intmem().\n",len);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp = (int *)malloc(len * sizeof(int));
+    if(tmp != NULL){
+        memset (tmp, 0, len * sizeof(int));
+        return (tmp);
+    } else{
+        fprintf(stderr,"ERROR: Cannot allocate memory for array allocate_intmem().\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//server util funcs
+void receiveFile(int sockfd){
+    char buffer[BUFFER_SIZE];
+    FILE *fp = fopen("myconfig.json","w");
+    if(fp == NULL){
+        printf("There is an error in opening the file.");
+        return exit(EXIT_FAILURE);
+    }
+
+    while(read(sockfd, buffer, BUFFER_SIZE) > 0) {
+        fprintf(fp,"%s", buffer);
+    }
+    printf("Config File received!\n");
+    fclose(fp);
+} 
+
 
 int main(int argc, char **argv) {
     FILE * fp;
